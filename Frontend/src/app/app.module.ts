d999a5703f384c5fd9af348fa6f43dbe1bf21631
amanvr2 <amanvr2@gmail.com>
2022-05-17 11:01:14 -0400

    first

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property-card/property-card.component';
+import { PropertyListComponent } from './property-list/property-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      PropertyCardComponent,
+      PropertyListComponent,
+      NavbarComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
